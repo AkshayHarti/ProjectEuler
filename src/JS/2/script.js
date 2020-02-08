ed0a2ea1183453c5cd141dc44d54abbf0b4df738
Akshay Harti <akshay.r555@gmail.com>
2020-02-08 21:59:11 +1100

    Completed problem 2

--- src/JS/2/script.js
+++ src/JS/2/script.js
@@ -1,46 +1,59 @@
-const target = 32
+const target = 4000000
 let startDate = new Date()
 let sum = mySolution(target)
-// sum = solutionByPE(target)
+// let sum = solutionByPE(target)
 let endDate = new Date()
 
 // Performance: O(n)
 /***************************My solution**************************/
 
-function fibonacci() {
-	if (memo[num]) return memo[num]
-	if (num == 0) return 1
-	if (num == 1) return 2
-
-	return (memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo))
-}
-
 function evenFibonacciSum(limit) {
 	const memo = [1, 2]
-	const sum = 1
-	const i = 1
+	let sum = memo[1]
+	let i = 2, n = 1
 
 	while (sum <= limit) {
-		memo[i + 1] = memo[i] + memo[i - 1]
+		memo[i] = memo[i - 1] + memo[i - 2]
+
+		if(memo[i] > limit) {
+			break
+		}
+
+		if(i === (3 * n + 1)) {
+			sum += memo[i]
+			n++
+		}	
+
+		i++
 	}
+
+	return sum
 }
 
 function mySolution(target = 0) {
-	console.log(fibonacci(target))
+	return evenFibonacciSum(target)
 }
 
-// Performance: Constant time O(1)
+// Performance: Constant time O(n)
 /***************************solution by Project Euler**************************/
-function divisibleBy(n, tempTarget = 0) {
-	const target = tempTarget - 1
-	const nthValue = parseInt(target / n)
-	const sumOf_n_naturalNumbers = (n * (nthValue * (nthValue + 1))) / 2
+function evenFibonacciSum_PE(limit) {
+	let a = 1
+	let b = 1
+	let c = a + b
+	let sum = 0
 
-	return sumOf_n_naturalNumbers
+	while (sum <= limit) {
+		sum += c
+		a = b + c
+		b = c + a
+		c = a + b
+	}
+
+	return sum
 }
 
 function solutionByPE(target) {
-	return divisibleBy(3, target) + divisibleBy(5, target) - divisibleBy(3 * 5, target)
+	return evenFibonacciSum_PE(target)
 }
 
 /*****************************************************/
